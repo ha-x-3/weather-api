3342c34e839579222648fd5e8b0c2e6e33d50f9d
Brittney Epperson <beppers87@hotmail.com>
2024-06-06 12:21:32 -0500

    Current temp displayed on page

--- scripts.js
+++ scripts.js
@@ -1,10 +1,12 @@
+const currentTemp = document.getElementById('currentTemp');
+
 async function getCurrentWeather() {
     const response = await fetch(
 		'http://api.weatherapi.com/v1/forecast.json?key=e53052be24db4c8abb1154941240606&q=62002&days=3&aqi=no&alerts=yes',
 		{ mode: 'cors' }
 	);
     const currentWeatherData = await response.json();
-    console.log(currentWeatherData);
+    currentTemp.innerText = `The current temperature is ${currentWeatherData.current.temp_f}\u00B0F.`;
 }
 
 getCurrentWeather();
