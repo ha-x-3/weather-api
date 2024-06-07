7c3647c4e1111b75ee998336f4a47d8fd43295e1
Brittney Epperson <beppers87@hotmail.com>
2024-06-07 10:51:25 -0500

    Refactored to use search string to update weather information

--- scripts.js
+++ scripts.js
@@ -10,31 +10,40 @@ const forecastDay3 = document.getElementById('forecastDay3');
 const searchInput = document.getElementById('location-search');
 const searchButton = document.querySelector('#searchDiv button');
 
-async function getCurrentWeather() {
-    const response = await fetch(
-		'http://api.weatherapi.com/v1/forecast.json?key=e53052be24db4c8abb1154941240606&q=62002&days=3&aqi=no&alerts=yes',
-		{ mode: 'cors' }
-	);
-    const currentWeatherData = await response.json();
-    console.log(currentWeatherData);
+async function fetchWeatherData(location) {
+	const apiKey = 'e53052be24db4c8abb1154941240606';
+	const apiUrl = `http://api.weatherapi.com/v1/forecast.json?key=${apiKey}&q=${location}&days=3&aqi=no&alerts=yes`;
 
-    currentLocation.innerHTML = `${currentWeatherData.location.name}, ${currentWeatherData.location.region}`;
-    currentTemp.innerText = `${currentWeatherData.current.temp_f}\u00B0F`;
-    currentConditionImage.src = currentWeatherData.current.condition.icon;
-    currentConditionText.innerText = currentWeatherData.current.condition.text;
-    feelsLikeTemp.innerHTML = `Feels like: ${currentWeatherData.current.feelslike_f}\u00B0F`;
-    lastUpdated.innerText = `Last updated: ${currentWeatherData.current.last_updated}`;
+	try {
+		const response = await fetch(apiUrl, { mode: 'cors' });
+		const currentWeatherData = await response.json();
+		console.log(currentWeatherData);
 
-    const forecast = currentWeatherData.forecast.forecastday;
-	updateForecastDay(forecastDay1, forecast[0]);
-	updateForecastDay(forecastDay2, forecast[1]);
-	updateForecastDay(forecastDay3, forecast[2]);
+		updateCurrentWeather(currentWeatherData);
+
+		const forecast = currentWeatherData.forecast.forecastday;
+		updateForecastDay(forecastDay1, forecast[0]);
+		updateForecastDay(forecastDay2, forecast[1]);
+		updateForecastDay(forecastDay3, forecast[2]);
+	} catch (error) {
+		console.error('Error fetching weather data:', error);
+	}
+}
+
+function updateCurrentWeather(weatherData) {
+	currentLocation.innerHTML = `${weatherData.location.name}, ${weatherData.location.region}`;
+	currentTemp.innerText = `${weatherData.current.temp_f}\u00B0F`;
+	currentConditionImage.src = weatherData.current.condition.icon;
+	currentConditionText.innerText = weatherData.current.condition.text;
+	feelsLikeTemp.innerHTML = `Feels like: ${weatherData.current.feelslike_f}\u00B0F`;
+	lastUpdated.innerText = `Last updated: ${weatherData.current.last_updated}`;
 }
 
 function updateForecastDay(dayDiv, forecastData) {
 	const dateString = forecastData.date;
 	const [year, month, day] = dateString.split('-');
 	const formattedDate = new Date(year, month - 1, day).toLocaleDateString();
+
 	const maxTemp = forecastData.day.maxtemp_f;
 	const minTemp = forecastData.day.mintemp_f;
 	const conditionIcon = forecastData.day.condition.icon;
@@ -49,4 +58,9 @@ function updateForecastDay(dayDiv, forecastData) {
     `;
 }
 
-getCurrentWeather();
+searchButton.addEventListener('click', () => {
+	const userInput = searchInput.value;
+	fetchWeatherData(userInput);
+});
+
+fetchWeatherData('62002'); //Search Alton, IL by default
